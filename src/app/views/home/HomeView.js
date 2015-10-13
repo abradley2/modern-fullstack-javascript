2b366d93eec92ff236351399adb14ba743d0dc96
tony <antbradley91@gmail.com>
2015-10-13 19:13:59 -0400

    update

--- src/app/views/home/HomeView.js
+++ src/app/views/home/HomeView.js
@@ -5,18 +5,18 @@ var HomeView = Backbone.View.extend({
     template: require('./homeView.jade'),
     post: marked(require('./home.md')),
 
-    initialize: function(){
-
-    },
-
     render: function(){
       this.$el.html(this.template({
         post: this.post
       }));
-      //React.render(<Example/>, this.$el.find('#example-component')[0]);
+      React.render(
+        <Example/>,
+        this.$el.find('#example-component')[0]
+      );
     },
 
     remove: function(){
+      React.unmountComponentAtNode(this.$el.find('#example-component')[0]);
       this.stopListening();
       this.$el.empty();
     }
